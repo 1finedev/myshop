2ebac93f29f8f9dfea03bb85e980411e1873ee73
Jalen Greene <terrell.greene95@gmail.com>
2020-09-08 21:15:15 -0500

    Loading modal component

---
+++ src/components/loading-modal/loading-modal.styles.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  indicatorContainer: {
+    backgroundColor: 'black',
+    opacity: 0.8,
+    // height: 40,
+    // width: 40,
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderRadius: 10,
+  },
+});
---
+++ src/components/loading-modal/loading-modal.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Modal, View, ActivityIndicator} from 'react-native';
+
+import styles from './loading-modal.styles';
+import {useGetLoadingState} from '../../redux/loadingSlice/loadingSlice.selectors';
+
+const LoadingModal: React.FC = () => {
+  const {isLoading} = useGetLoadingState();
+  return (
+    <Modal visible={isLoading} transparent>
+      <View style={styles.container}>
+        <View style={styles.indicatorContainer}>
+          <ActivityIndicator size="large" />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+export default LoadingModal;
