f3607f8e065b44f38983cd5b008fed0cfbb6fd4d
Jalen Greene <terrell.greene95@gmail.com>
2020-09-08 21:14:00 -0500

    adding provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
-import {RootNavigator} from './navigation';
+import {Provider} from 'react-redux';
 
 import {useCachedResources} from './utils';
+import {RootNavigator} from './navigation';
 import {SplashScreen} from './screens';
+import store from './redux';
 // console.log(Constants.systemFonts);
 
 const App = () => {
@@ -13,10 +15,10 @@ const App = () => {
     return <SplashScreen />;
   } else {
     return (
-      <>
+      <Provider store={store}>
         <StatusBar barStyle="dark-content" />
         <RootNavigator />
-      </>
+      </Provider>
     );
   }
 };
