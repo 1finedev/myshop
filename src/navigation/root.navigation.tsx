9f54059ebec52726060ba41f16d5ae0d41e9eb1c
Jalen Greene <terrell.greene95@gmail.com>
2020-09-08 21:18:15 -0500

    Loading Modal added to app

--- src/navigation/root.navigation.tsx
+++ src/navigation/root.navigation.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthNavigator} from './auth.navigation';
+import {LoadingModal} from '../components';
 
 export const RootNavigator: React.FC = () => {
   return (
     <NavigationContainer>
+      <LoadingModal />
       <AuthNavigator />
     </NavigationContainer>
   );
