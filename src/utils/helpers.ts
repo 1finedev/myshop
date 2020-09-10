5ce77fe635e57bdaf138eb1d21e14bc702efce80
Jalen Greene <terrell.greene95@gmail.com>
2020-09-10 12:10:12 -0500

    timeout async func

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,7 @@
+export const timeout = (ms: number): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 export const hasUpperCase = (str?: string): boolean => {
   return str ? str.toLowerCase() !== str : false;
 };
